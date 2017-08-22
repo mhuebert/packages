ad3f9bba4fbee336a79049ec8b08dd703e2b15c3
Matt Huebert <mhuebert@gmail.com>
2017-08-22 10:31:25 +0200

    Update externs list
    Some externs were missing, others were listed but not in the jar.

--- firebase/resources/deps.cljs
+++ firebase/resources/deps.cljs
@@ -2,9 +2,12 @@
                  :provides ["cljsjs.firebase"]}
                 {:file "cljsjs/development/firebase-node.inc.js"
                  :provides ["cljsjs.firebase-node"]}]
- :externs ["cljsjs/common/firebase-database-externs.js"
-           "cljsjs/common/firebase-storage-externs.js"
+ :externs ["cljsjs/common/firebase-app-externs.js"
            "cljsjs/common/firebase-auth-externs.js"
-           "cljsjs/common/firebase-server-auth-externs.js"
            "cljsjs/common/firebase-client-auth-externs.js"
-           "cljsjs/common/firebase-app-externs.js"]}
+           "cljsjs/common/firebase-database-externs.js"
+           "cljsjs/common/firebase-error-externs.js"
+           "cljsjs/common/firebase-externs.js"
+           "cljsjs/common/firebase-messaging-externs.js"
+           "cljsjs/common/firebase-storage-externs.js"
+           ]}
